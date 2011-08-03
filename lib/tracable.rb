eca2a28b82f90bebea55a5c5dcc311f16d5b8171
Andrew Liu <andrewliu33@gmail.com>
2011-08-03 11:21:50 +0800

    Adding prefix to instance variables to prevent naming collision

--- lib/tracable.rb
+++ lib/tracable.rb
@@ -10,12 +10,12 @@ module Defly
     end
     
     def trace_method!(methods)
-      @defly_methods ||= []
+      @__defly_methods ||= []
       
       methods = public_methods(false) if methods.size == 0
       methods.map! {|m| m.to_sym }
-      methods -= @defly_methods
-      @defly_methods |= methods
+      methods -= @__defly_methods
+      @__defly_methods |= methods
       
       STDERR << "Tracing #{methods.join(', ')} on #{self.class} instance\n"
       
@@ -30,7 +30,7 @@ module Defly
               @__defly_level += 1
               result = super
               @__defly_level -= 1
-              defly_check_var(indent) if defined? defly_check_var
+              __defly_check_var(indent) if defined? __defly_check_var
               STDERR << "\#{indent}>>>>> \#{result}\n"
               result
             rescue
@@ -43,37 +43,28 @@ module Defly
     end
     
     def trace_variable!(variables)
-      @defly_variables ||= []
+      @__defly_variables ||= []
       
       variables = instance_variables if variables.size == 0
       variables.map! {|v| v.to_sym }
-      variables -= @defly_variables
-      @defly_variables |= variables
+      variables -= @__defly_variables
+      @__defly_variables |= variables
       
-      STDERR << "Tracing #{variables.join(', ')}  on #{self.class} instance\n"
+      STDERR << "Tracing #{variables.join(', ')} on #{self.class} instance\n"
       
-      eigenclass.remove_method :defly_check_var_list if eigenclass.instance_methods.include? :defly_check_var_list
-      
-      eigenclass.class_eval <<-RUBY, __FILE__, __LINE__
-        def defly_check_var_list
-          [#{@defly_variables.map(&:inspect).join(', ')}]
-        end
-      RUBY
-      
-      unless eigenclass.instance_methods.include? :defly_check_var
+      unless eigenclass.instance_methods.include? :__defly_check_var
         eigenclass.class_eval <<-RUBY, __FILE__, __LINE__
-          def defly_check_var(indent)
-            @__defly ||= {}
+          def __defly_check_var(indent)
+            @__defly_varmap ||= {}
             
-            var_list = defly_check_var_list
-            var_list.each do |v|
+            @__defly_variables.each do |v|
               new_val = instance_variable_get(v)
-              if @__defly[v]
-                STDERR << "    \#{indent}\#{v.to_s} = \#{new_val.inspect} # \#{@__defly[v].inspect} -> \#{new_val.inspect}\n" unless @__defly[v] == new_val
+              if @__defly_varmap[v]
+                STDERR << "    \#{indent}\#{v.to_s} = \#{new_val.inspect} # \#{@__defly_varmap[v].inspect} -> \#{new_val.inspect}\n" unless @__defly_varmap[v] == new_val
               else
                 STDERR << "    \#{indent}\#{v.to_s} = \#{new_val.inspect} # undefined\n"
               end
-              @__defly[v] = new_val
+              @__defly_varmap[v] = new_val
             end
           end
         RUBY
@@ -81,7 +72,7 @@ module Defly
     end
     
     def untrace!
-      [:defly_check_var_list, :defly_check_var].concat(@defly_methods) do |m|
+      [:defly_check_var].concat(@__defly_methods) do |m|
         eigenclass.remove_method(m) if eigenclass.instance_methods.include? m
       end
     end
