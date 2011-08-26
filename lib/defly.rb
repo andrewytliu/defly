e4093d5a2ad1c767487f28570b4a0cdef90566d3
Andrew Liu <andrewliu33@gmail.com>
2011-08-26 16:58:28 +0800

    Add debug! method to hook up

--- lib/defly.rb
+++ lib/defly.rb
@@ -1,7 +1,15 @@
 require 'tracable'
 require 'whinable'
+require 'inspectable'
 
 module Defly
+end
+
+class Class
+  def debug!
+    self.send(:include, Defly::Inspectable)
+    self.send(:include, Defly::Tracable)
+  end
 end
 
 NoMethodError.send(:include, Defly::Whinable)
